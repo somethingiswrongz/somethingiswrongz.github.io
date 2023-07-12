293057ecfff456f9759f0e675d89b5031c5fa26e
somethingiswrongz <learningrooney898@gmail.com>
2023-07-12 21:07:57 +0700

    add files

---
+++ index.js
@@ -0,0 +1,7 @@
+var moment = require('moment');
+
+console.log("Hello from JavaScript!");  
+console.log(moment().startOf('day').fromNow());
+
+var name = "Bob", time = "today";  
+console.log(`Hello ${name}, how are you ${time}?`);
